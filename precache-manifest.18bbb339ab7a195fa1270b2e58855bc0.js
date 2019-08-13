c5c180cc9fc2e3cf0a79f071ceef637d63cc7f92
Iana Godskesen Kalinichenko <iana-git@outlook.com>
2019-08-13 15:36:33 +0200

    Updates

---
+++ precache-manifest.18bbb339ab7a195fa1270b2e58855bc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5c23af08277f6f0afb67a2111b831e",
+    "url": "/calya/index.html"
+  },
+  {
+    "revision": "611a6690bb5608141b3b",
+    "url": "/calya/static/css/main.c2af609a.chunk.css"
+  },
+  {
+    "revision": "c9b76ea940f1b7789feb",
+    "url": "/calya/static/js/2.7cdb11dc.chunk.js"
+  },
+  {
+    "revision": "611a6690bb5608141b3b",
+    "url": "/calya/static/js/main.f52d6cf2.chunk.js"
+  },
+  {
+    "revision": "05af2cfdcb4ae61f6004",
+    "url": "/calya/static/js/runtime~main.5d21853e.js"
+  }
+]);
